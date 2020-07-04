c6c670d3211f7194d4d7a8bf8297b8e30f25ea96
Caio Magalhães <caio.lmf@gmail.com>
2020-07-03 23:11:47 -0300

    Refac for CC

--- src/components/content/VideoPage/VideoPlayer/VideoLike.js
+++ src/components/content/VideoPage/VideoPlayer/VideoLike.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { getItem, addItem, removeItem } from '../../../../service/localStorage';
+import React, { Component } from "react";
+import { getItem, addItem, removeItem } from "../../../../service/localStorage";
 
 class VideoLike extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-      isLiked: null, 
-      likeCount: Number(props.statistics.likeCount), 
+    this.state = {
+      isLiked: null,
+      likeCount: Number(props.statistics.likeCount),
       isDisliked: null,
       dislikeCount: Number(props.statistics.dislikeCount),
     };
@@ -17,15 +17,15 @@ class VideoLike extends Component {
 
   componentDidMount() {
     const { videoId } = this.props;
-    const videosLike = getItem('videosLike', videoId);
+    const videosLike = getItem("videosLike", videoId);
 
     if (videosLike) {
       const likeOrDislike = videosLike.find((video) => video.id === videoId);
       if (likeOrDislike) {
         if (likeOrDislike.like) {
-          this.handleVideoLike('like');
+          this.handleVideoLike("like");
         } else if (likeOrDislike.Dislike) {
-          this.handleVideoLike('dislike');
+          this.handleVideoLike("dislike");
         }
       }
     }
@@ -35,61 +35,63 @@ class VideoLike extends Component {
     const { videoId, statistics } = this.props;
     const { isLiked, isDisliked } = this.state;
     const originalLikes = Number(statistics.likeCount);
-    const originalDislikes = Number(statistics.dislikeCount)
+    const originalDislikes = Number(statistics.dislikeCount);
 
-    switch (action) {
-      case 'like':
-        this.setState({ isLiked: !isLiked, isDisliked: false }, () => {
-          removeItem('videosLike', { id: videoId });
-          if (!isLiked) {
-            addItem('videosLike', { id: videoId, like: true });
-            return this.setState(
-              { likeCount: originalLikes + 1, dislikeCount: originalDislikes },
-            )
-          }
-          this.setState({likeCount: originalLikes});
+    this.setState({ isLiked: !isLiked, isDisliked: false }, () => {
+      removeItem("videosLike", { id: videoId });
+      if (!isLiked) {
+        addItem("videosLike", { id: videoId, like: true });
+        return this.setState({
+          likeCount: originalLikes + 1,
+          dislikeCount: originalDislikes,
         });
-        break;
-      case 'dislike':
-        this.setState({ isLiked: false, isDisliked: !isDisliked }, () => {
-          removeItem('videosLike', { id: videoId });
-          if (!isDisliked) {
-            addItem('videosLike', { id: videoId, Dislike: true });
-            return this.setState(
-              { likeCount: originalLikes, dislikeCount: originalDislikes + 1 },
-            )
-          }
-          this.setState({ dislikeCount: originalDislikes });
+      }
+      this.setState({ likeCount: originalLikes });
+    });
+  }
+
+  handleVideoDislike() {
+    const { videoId, statistics } = this.props;
+    const { isLiked, isDisliked } = this.state;
+    const originalLikes = Number(statistics.likeCount);
+    const originalDislikes = Number(statistics.dislikeCount);
+
+    this.setState({ isLiked: false, isDisliked: !isDisliked }, () => {
+      removeItem("videosLike", { id: videoId });
+      if (!isDisliked) {
+        addItem("videosLike", { id: videoId, Dislike: true });
+        return this.setState({
+          likeCount: originalLikes,
+          dislikeCount: originalDislikes + 1,
         });
-        break;
-      default:
-        break;
-    }
+      }
+      this.setState({ dislikeCount: originalDislikes });
+    });
   }
 
   render() {
     const { isLiked, likeCount, isDisliked, dislikeCount } = this.state;
 
     return (
-      <div className='thumb-wrapper'>
+      <div className="thumb-wrapper">
         <a
-          className='thumb-up-btn'
-          onClick={() => this.handleVideoLike('like')}
+          className="thumb-up-btn"
+          onClick={() => this.handleVideoLike()}
         >
-          <i className={`material-icons ${isLiked && 'thumb-selected'}`}>
+          <i className={`material-icons ${isLiked && "thumb-selected"}`}>
             thumb_up
           </i>
-          <span className='thumbs-count'>{likeCount}</span>
+          <span className="thumbs-count">{likeCount}</span>
         </a>
 
         <a
-          className='thumb-down-btn'
-          onClick={() => this.handleVideoLike('dislike')}
+          className="thumb-down-btn"
+          onClick={() => this.handleVideoDislike()}
         >
-          <i className={`material-icons ${isDisliked && 'thumb-selected'}`}>
+          <i className={`material-icons ${isDisliked && "thumb-selected"}`}>
             thumb_down
           </i>
-          <span className='thumbs-count'>{dislikeCount}</span>
+          <span className="thumbs-count">{dislikeCount}</span>
         </a>
       </div>
     );
